baf98dadf8e36b48f3e79c13cbac26552f0f96b1
Jason Carucci <jcarucci@gmail.com>
2020-02-24 21:32:23 -0500

    Add UIPickerView

--- UIPickerViewPlay/UIPickerViewPlay/ViewController.swift
+++ UIPickerViewPlay/UIPickerViewPlay/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    var data = [ "Boulez", "Beethoven", "Brahms" ]
+    var picker = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.dataSource = self
+        textField.inputView = picker
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textField.text = data[row]
+        //textField.resignFirstResponder()
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
 }
 
